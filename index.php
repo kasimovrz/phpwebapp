b4900595f663b241255116b23ebbea6641c259c2
kasimovrz <122051566+kasimovrz@users.noreply.github.com>
2023-01-10 01:38:07 +0600

    4

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>3</title>
+    <title>4</title>
 </head>
 <body>
     <h1>ping <?php
@@ -12,10 +12,9 @@
     ?></h1>
     <?php
         $link = mysql_connect('phpdb.mysql.database.azure.com', 'phpadmin', 'Asdf-1234');
-            if (!$link) {
-                die('Ошибка соединения: ' . mysql_error());
+            if ($link) {
+                echo 'Успешно соединились';
             }
-        echo 'Успешно соединились';
         mysql_close($link);
     ?>
     
