a6c38140af58fcfcba09d683ebf7f5f388be18cd
kasimovrz <122051566+kasimovrz@users.noreply.github.com>
2023-01-10 01:52:42 +0600

    5

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4</title>
+    <title>5</title>
 </head>
 <body>
     <h1>ping <?php
         echo 'pong';
     ?></h1>
     <?php
-        $link = mysql_connect('phpdb.mysql.database.azure.com', 'phpadmin', 'Asdf-1234');
+        $link = mysql_connect('phpdb.mysql.database.azure.com', 'phpadmin', 'Asdf-1234', 'mydb');
             if ($link) {
                 echo 'Успешно соединились';
             }
