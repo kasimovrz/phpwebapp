7b76064124ff0d3ce388a235472b0ac2e263c740
kasimovrz <122051566+kasimovrz@users.noreply.github.com>
2023-01-10 01:11:44 +0600

    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $link = mysql_connect('phpdb.mysql.database.azure.com', 'phpadmin', 'Asdf-1234');
+            if (!$link) {
+                die('Ошибка соединения: ' . mysql_error());
+            }
+        echo 'Успешно соединились';
+        mysql_close($link);
+    ?>
+</body>
+</html>
