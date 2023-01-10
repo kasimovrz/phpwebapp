8bfd0063e70cdd87243a3a445ca6acd2199f684d
kasimovrz <122051566+kasimovrz@users.noreply.github.com>
2023-01-10 21:00:49 +0600

    10

--- index.php
+++ index.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>9</title>
+    <title>10</title>
 </head>
 <body>
     <h1>ping <?php
         echo 'pong';
     ?></h1>
-    <?php
+    <h1><?php
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:server-ilyas.database.windows.net,1433; Database = AdventureWorks", "sqladmin", "Asdf-1234");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
@@ -25,7 +26,11 @@ catch (PDOException $e) {
 $connectionInfo = array("UID" => "sqladmin", "pwd" => "{your_password_here}", "Database" => "AdventureWorks", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:server-ilyas.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
+    if($conn){
+        echo 'go';
+    }
+
+?></h1>
     
 </body>
 </html>
